c436de5025999002c2e246c6644505936ecf8936
whalestar2 <162273958+whalestar2@users.noreply.github.com>
2024-06-14 09:27:37 +0900

    #12-DB
    어플리케이션(홈커트롤러) 개념 배움

--- src/main/java/com/bitstudy/board/BoardApplication.java
+++ src/main/java/com/bitstudy/board/BoardApplication.java
@@ -9,6 +9,10 @@ public class BoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
+	//이 어플리케이션 전체 구동시켜라(프로젝트 board 전체를 어플리케이션이라고 함) 
+	//무조건 도는 거. ( 테스트 할 때도 이게 돌고 난 후 테스트 함) 
+	// drop table, create table 하고 데이터 넣고 다 함
+	// 스프링의 HomeController 와 같은 거.
 
 //사용자한테 정보를 받아서 실행
 }
